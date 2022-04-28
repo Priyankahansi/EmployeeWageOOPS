7b8dada02813824fe52f0374a92c2ab901f0e3ae
Priyanka <priyankahansi95@gmail.com>
2022-04-28 23:18:41 +0530

    added:employee wage using switch cases

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,10 +2,11 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
-    static int IS_FULL_TIME = 1;
     static int EMP_RATE_PER_HOUR = 20;
     static int empHrs = 0;
     static int empWage = 0;
+    static final int IS_PART_TIME = 1;
+    static final int IS_FULL_TIME = 2;
 
     //UC1
     static void method1() {
@@ -30,8 +31,7 @@ public class EmployeeWage {
 
     //UC3
     static void partTimeEmployeeWage() {
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
+
         double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_PART_TIME)
             empHrs = 4;
@@ -41,12 +41,33 @@ public class EmployeeWage {
             empHrs = 0;
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp wage:" + empWage);
+    }
+
+    static void employeeWageSwitchStatements() {
+        int empHrs = 0;
+        int empWage = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage:" + empWage);
 
     }
+
     public static void main(String[] args) {
         EmployeeWage obj2 = new EmployeeWage();
         method1();//calling first method
         obj2.dailyEmployeeWage();//calling second method UC2.
         partTimeEmployeeWage();//calling third method UC3.
+        employeeWageSwitchStatements();
     }
 }
