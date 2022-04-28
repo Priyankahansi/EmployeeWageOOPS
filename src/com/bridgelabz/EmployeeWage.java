dafdc63b045cd7e6261ebd36cff5ec1ad574cce6
Priyanka <priyankahansi95@gmail.com>
2022-04-28 22:36:26 +0530

    added:Daily employee wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,7 +3,11 @@ package com.bridgelabz;
 public class EmployeeWage {
 
     static int IS_FULL_TIME = 1;
+    static int EMP_RATE_PER_HOUR = 20;
+    static int empHrs = 0;
+    static int empWage = 0;
 
+    //UC1
     static void method1() {
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME) {
@@ -11,10 +15,21 @@ public class EmployeeWage {
         } else {
             System.out.println("Employee is absent");
         }
-
     }
 
+    //creating another method UC2
+    void dailyEmployeeWage() {
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage:" + empWage);
+    }
     public static void main(String[] args) {
-        method1();
+        EmployeeWage obj2=new EmployeeWage();
+        method1();//calling first method
+        obj2.dailyEmployeeWage();//calling second method UC2.
     }
 }
