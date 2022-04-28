fcac811fad36b35b9f3e36c1a1337ecbb81800bd
Priyanka <priyankahansi95@gmail.com>
2022-04-28 22:24:13 +0530

    added:employee attendence

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,10 +1,18 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
- static void method1()
- {
-     System.out.println("welcome to employee wage program");
- }
+
+    static int IS_FULL_TIME = 1;
+
+    static void method1() {
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
+
+    }
 
     public static void main(String[] args) {
         method1();
