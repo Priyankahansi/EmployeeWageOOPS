691558d9daf6d595952fc4d6b8998eff4fbef2a2
Priyanka <priyankahansi95@gmail.com>
2022-04-28 22:08:43 +0530

    welcome to employee wage

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+ static void method1()
+ {
+     System.out.println("welcome to employee wage program");
+ }
+
+    public static void main(String[] args) {
+        method1();
+    }
+}
